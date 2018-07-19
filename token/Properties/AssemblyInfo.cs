67e8f874b1b9211455f27d80286edaa542a2ff91
ayliincaliskan <aylin.03975@gmail.com>
2018-07-19 14:41:12 +0300

    xbf

---
+++ token/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("token")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("token")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d6f69ec-792a-4521-b23d-35e6de39318c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ token/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetOpenX.Rest.Client;
+using NetOpenX.Rest.Client.Model;
+
+namespace toplu
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static oAuth2 _oAuth2;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _oAuth2 = new oAuth2("http://localhost:7070");
+            _oAuth2.Login(new JLogin()
+            {
+                BranchCode = 0,
+                NetsisUser = "netsis",
+                NetsisPassword = "1",
+                DbType = JNVTTipi.vtMSSQL,
+                DbName = "ENTERPRISE9",
+                DbPassword = "sapass",
+                DbUser = "TEMELSET"
+            });
+            string token = _oAuth2.AccessToken;
+
+        }
+    }
+}
